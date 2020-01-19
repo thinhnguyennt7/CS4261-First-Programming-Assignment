a24b5a548770462848919580284761ca0173c2bf
Tuan Nguyen <atuannguyen1101@gmail.com>
2020-01-19 04:53:46 -0500

    Simple front end

--- client-side/src/app/app.module.ts
+++ client-side/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import { HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatCardModule,
+    MatTabsModule,
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   exports: [
     MatInputModule,
--- client-side/src/app/login/login.component.ts
+++ client-side/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientService } from '../http-client.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public tab: string = "login";
+  public username: string;
+  public password: string;
+
+  constructor(private httpClient: HttpClientService) { }
 
   ngOnInit() {
   }
 
+  loginTabClick(event: any): void {
+    if (event.index == 0) {
+      this.tab = "login";
+    } else if (event.index == 1) {
+      this.tab = "register";
+    }
+  }
+
+  reset(): void {
+    this.username = "";
+    this.password = "";
+  }
+
+  login() {
+    // console.log(this.tab);
+    // console.log(this.username);
+    // console.log(this.password)
+    this.httpClient.get("/v1/account").subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
