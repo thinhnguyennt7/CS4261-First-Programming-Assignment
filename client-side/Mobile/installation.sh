ab95be00e199d50d5f48513b09ed2fbc38150500
Thinh Nguyen <tnntech@gatech.edu>
2020-01-21 22:37:48 -0500

    add bash script for pre-install. Update read me follow with instruction to start mobile app

---
+++ client-side/Mobile/installation.sh
@@ -0,0 +1,4 @@
+npm install
+sudo gem install cocoapods
+react-native link react-native-vector-icons
+cd ios && pod install && cd ..
---
+++ client-side/Mobile/ios/build/Mobile/Build/Intermediates.noindex/Mobile.build/Debug-iphonesimulator/Mobile.build/Script-0DC51DCB9D6B62C3EE797C94.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-Mobile/Pods-Mobile-resources.sh"
+
